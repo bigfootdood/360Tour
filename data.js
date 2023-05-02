804452df02f4050d259f7c1a381242c2ed08b6a4
bigfootdood <bigfootdood@gmail.com>
2023-05-01 21:45:42 -0400

    Addes Suprema Layout Change

--- data.js
+++ data.js
@@ -1,8 +1,54 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-0001",
-      "name": "0001",
+      "id": "0-suprema",
+      "name": "Suprema",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.056829090000004356,
+        "pitch": 0,
+        "fov": 1.4450773171985711
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.629508790447373,
+          "pitch": 0.6758219780019434,
+          "rotation": 0,
+          "target": "1-suprema-market"
+        },
+        {
+          "yaw": 0.5053578157138823,
+          "pitch": 0.035652100259918384,
+          "rotation": 0,
+          "target": "7-kitchenprep"
+        },
+        {
+          "yaw": 0.14811235344595453,
+          "pitch": 0.00879965251654724,
+          "rotation": 0,
+          "target": "6-restroomregister"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-suprema-market",
+      "name": "Suprema Market",
       "levels": [
         {
           "tileSize": 256,
@@ -26,41 +72,35 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 0.009868272747675988,
-          "pitch": 0.09090505076434674,
+          "yaw": -3.1054358243275217,
+          "pitch": 0.706423011668921,
           "rotation": 0,
-          "target": "1-0002"
+          "target": "0-suprema"
         },
         {
-          "yaw": -0.9464089164599656,
-          "pitch": 0.11027551381748246,
+          "yaw": -0.7270308332519182,
+          "pitch": 0.2821800369114733,
           "rotation": 0,
-          "target": "2-0003"
+          "target": "3-grabandgo"
         },
         {
-          "yaw": 1.2517964441438938,
-          "pitch": 0.1502100665005468,
+          "yaw": 0.05993634282612881,
+          "pitch": 0.1770375919215592,
           "rotation": 0,
-          "target": "3-0004"
+          "target": "2-counter"
         },
         {
-          "yaw": -0.002262108046487299,
-          "pitch": -0.914594400213339,
+          "yaw": 0.9630856541571369,
+          "pitch": 0.09591173666253106,
           "rotation": 0,
-          "target": "5-0006"
-        },
-        {
-          "yaw": -0.31791504490919387,
-          "pitch": 0.04513253307885812,
-          "rotation": 0,
-          "target": "4-0005"
+          "target": "4-butcher"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "1-0002",
-      "name": "0002",
+      "id": "2-counter",
+      "name": "Counter",
       "levels": [
         {
           "tileSize": 256,
@@ -84,41 +124,35 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": -0.5434535471564477,
-          "pitch": 0.098938294015543,
+          "yaw": -2.567188891304731,
+          "pitch": 0.06642139359396992,
           "rotation": 0,
-          "target": "4-0005"
+          "target": "3-grabandgo"
         },
         {
-          "yaw": -2.4846747685291373,
-          "pitch": 0.1725646621015411,
+          "yaw": 2.5701487280006257,
+          "pitch": 0.11770165250121245,
           "rotation": 0,
-          "target": "2-0003"
+          "target": "4-butcher"
         },
         {
-          "yaw": 2.5114925436713014,
-          "pitch": 0.16202346172728355,
+          "yaw": -3.1360214016051984,
+          "pitch": 0.21539859579523224,
           "rotation": 0,
-          "target": "3-0004"
+          "target": "1-suprema-market"
         },
         {
-          "yaw": -3.0737740874400625,
-          "pitch": 0.25418122098536955,
+          "yaw": -0.5588720251184824,
+          "pitch": 0.05527567719962789,
           "rotation": 0,
-          "target": "0-0001"
-        },
-        {
-          "yaw": -2.858968325349025,
-          "pitch": -1.4842312113209353,
-          "rotation": 0,
-          "target": "5-0006"
+          "target": "5-work-tables"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "2-0003",
-      "name": "0003",
+      "id": "3-grabandgo",
+      "name": "GrabAndGo",
       "levels": [
         {
           "tileSize": 256,
@@ -142,41 +176,41 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 1.779125615959333,
-          "pitch": 0.3454396150589041,
+          "yaw": 1.4532359046146572,
+          "pitch": 0.7104193886708163,
           "rotation": 0,
-          "target": "0-0001"
+          "target": "1-suprema-market"
         },
         {
-          "yaw": 0.4354469550006961,
-          "pitch": 0.16912150612970223,
+          "yaw": 1.3313170433608796,
+          "pitch": 0.09930366155803227,
           "rotation": 0,
-          "target": "1-0002"
+          "target": "4-butcher"
         },
         {
-          "yaw": 1.428811886991375,
-          "pitch": 0.17828666182872865,
+          "yaw": 0.4246467588906473,
+          "pitch": 0.16103586516090118,
           "rotation": 0,
-          "target": "3-0004"
+          "target": "2-counter"
         },
         {
-          "yaw": -0.0706387145437617,
-          "pitch": 0.08516813159697278,
+          "yaw": -0.0683668676218332,
+          "pitch": 0.027465556564942872,
           "rotation": 0,
-          "target": "4-0005"
+          "target": "5-work-tables"
         },
         {
-          "yaw": 0.6332966571624361,
-          "pitch": -0.43732291193636286,
+          "yaw": 2.428174282827716,
+          "pitch": 0.3780527240211491,
           "rotation": 0,
-          "target": "5-0006"
+          "target": "0-suprema"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "3-0004",
-      "name": "0004",
+      "id": "4-butcher",
+      "name": "Butcher",
       "levels": [
         {
           "tileSize": 256,
@@ -200,41 +234,41 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": -0.7133401439430749,
-          "pitch": 0.08296387289057705,
+          "yaw": -1.20448575688512,
+          "pitch": 0.5183125466220915,
           "rotation": 0,
-          "target": "2-0003"
+          "target": "1-suprema-market"
         },
         {
-          "yaw": -1.3204527357000124,
-          "pitch": 0.37341840634517,
+          "yaw": -1.2133478899294374,
+          "pitch": 0.037337809230205465,
           "rotation": 0,
-          "target": "0-0001"
+          "target": "3-grabandgo"
         },
         {
-          "yaw": -0.14513000357567307,
-          "pitch": 0.19819479531172135,
+          "yaw": -0.09062137911220525,
+          "pitch": 0.22397539718237525,
           "rotation": 0,
-          "target": "1-0002"
+          "target": "2-counter"
         },
         {
-          "yaw": 0.10516126329763864,
-          "pitch": -0.6690715658944484,
+          "yaw": -0.05185809680790676,
+          "pitch": -0.0030847642667168884,
           "rotation": 0,
-          "target": "5-0006"
+          "target": "5-work-tables"
         },
         {
-          "yaw": 0.05654683941156868,
-          "pitch": 0.0029216364166142483,
+          "yaw": -1.8044861132965266,
+          "pitch": 0.2883797280507743,
           "rotation": 0,
-          "target": "4-0005"
+          "target": "0-suprema"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "4-0005",
-      "name": "0005",
+      "id": "5-work-tables",
+      "name": "Work Tables",
       "levels": [
         {
           "tileSize": 256,
@@ -258,41 +292,35 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 0.20577979227616083,
-          "pitch": 0.19110099294305627,
+          "yaw": -0.025077993239865037,
+          "pitch": 0.24457431367519433,
           "rotation": 0,
-          "target": "1-0002"
+          "target": "2-counter"
         },
         {
-          "yaw": 0.3821648324070672,
-          "pitch": 0.09040978198498095,
+          "yaw": 0.6470505239888631,
+          "pitch": 0.07603528736407483,
           "rotation": 0,
-          "target": "0-0001"
+          "target": "3-grabandgo"
         },
         {
-          "yaw": 0.6466919147324468,
-          "pitch": 0.11293829519286369,
+          "yaw": 0.3334541712524306,
+          "pitch": 0.1085372451028892,
           "rotation": 0,
-          "target": "2-0003"
+          "target": "1-suprema-market"
         },
         {
-          "yaw": 0.06651911439944058,
-          "pitch": 0.04870613721561945,
+          "yaw": 0.06071798416723517,
+          "pitch": 0.012296213760350838,
           "rotation": 0,
-          "target": "3-0004"
-        },
-        {
-          "yaw": 0.08638713468630499,
-          "pitch": -0.6277233949204852,
-          "rotation": 0,
-          "target": "5-0006"
+          "target": "4-butcher"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "5-0006",
-      "name": "0006",
+      "id": "6-restroomregister",
+      "name": "Restroom/Register",
       "levels": [
         {
           "tileSize": 256,
@@ -310,46 +338,68 @@ var APP_DATA = {
       ],
       "faceSize": 1024,
       "initialViewParameters": {
-        "yaw": 1.5682614635805088,
-        "pitch": 1.5635192141647654,
-        "fov": 1.4463261352387993
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
       },
       "linkHotspots": [
         {
-          "yaw": -0.48688609644476166,
-          "pitch": 0.7118344474402019,
+          "yaw": -0.846095956729668,
+          "pitch": 0.08598087073315952,
           "rotation": 0,
-          "target": "4-0005"
+          "target": "7-kitchenprep"
         },
         {
-          "yaw": -0.043869230284194316,
-          "pitch": 1.2804939818245256,
+          "yaw": 0.45572888368726794,
+          "pitch": 0.029827870333125617,
           "rotation": 0,
-          "target": "1-0002"
+          "target": "0-suprema"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-kitchenprep",
+      "name": "Kitchen/Prep",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
         },
         {
-          "yaw": -2.433086559088281,
-          "pitch": 0.9245680750284002,
-          "rotation": 0,
-          "target": "2-0003"
+          "tileSize": 512,
+          "size": 512
         },
         {
-          "yaw": 2.638449284950606,
-          "pitch": 0.9120585196727937,
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.134774771923631,
+          "pitch": 0.017391966713592666,
           "rotation": 0,
-          "target": "3-0004"
+          "target": "6-restroomregister"
         },
         {
-          "yaw": -3.0717335828870063,
-          "pitch": 1.0490833159764374,
+          "yaw": -0.26302698789718804,
+          "pitch": 0.06717418102072159,
           "rotation": 0,
-          "target": "0-0001"
+          "target": "0-suprema"
         }
       ],
       "infoHotspots": []
     }
   ],
-  "name": "TestTour",
+  "name": "Suprema Market",
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": true,
